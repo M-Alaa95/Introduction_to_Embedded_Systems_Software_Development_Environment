93219eef88881e94064afdd64ffe91769603eefe
M-Alaa95 <m.alaafattah95@gmail.com>
2020-12-13 18:28:29 +0200

    Deleting all generated files for the host machine

--- C1M2/SRC/main.d
+++
@@ -1,6 +0,0 @@
-main.o: main.c ../INCLUDES/common/platform.h \
- c:\program\ files\ (x86)\codeblocks\mingw\include\stdio.h \
- c:\program\ files\ (x86)\codeblocks\mingw\include\_mingw.h \
- c:\program\ files\ (x86)\codeblocks\mingw\lib\gcc\mingw32\4.8.1\include\stddef.h \
- c:\program\ files\ (x86)\codeblocks\mingw\lib\gcc\mingw32\4.8.1\include\stdarg.h \
- ../INCLUDES/common/memory.h
